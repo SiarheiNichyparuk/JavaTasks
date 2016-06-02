a6f41683a4809bdc9640e989a9e9a25e017fa613
SiarheiNichyparuk <dk12@tut.by>
2016-06-02 10:19:10 +0300

    added changes

--- gmailTest/src/test/java/com/epam/automation/SampleJUnitTest.java
+++ gmailTest/src/test/java/com/epam/automation/SampleJUnitTest.java
@@ -12,7 +12,10 @@ import org.openqa.selenium.support.PageFactory;
 import com.epam.automation.pages.HomePage;
 
 public class SampleJUnitTest extends JUnitTestBase {
-	private static final String USER_1 = "TestAccount";
+	private static final String USER_1 = "CrazyRabbit322";
+	private static final String USER_1_PASSWD = "H3jAZmc09j7";
+	private static final String USER_2 = "sergeseger";
+	private static final String USER_2_PASSWD = "sergesege";
 
 
 
@@ -20,11 +23,25 @@ public class SampleJUnitTest extends JUnitTestBase {
   public void testHomePageHasAHeader() {
 	  WebDriver driver = new FirefoxDriver();
 	  driver.navigate().to("http://www.gmail.com");
-	  WebElement mailString = driver.findElement(By.name("service"));
-	  mailString.click();
+	  WebElement login = driver.findElement(By.id("Email"));
+	  login.click();
+	  login.sendKeys(USER_1);
+	  login.submit();
+	  WebElement next = driver.findElement(By.id("next"));
+	  next.click();
+	  WebElement password = driver.findElement(By.id("Passwd"));
+	  password.click();
+	  password.sendKeys(USER_1_PASSWD);
+	  WebElement signIn = driver.findElement(By.id("signIn"));
+	  signIn.click();
+	  WebElement write = driver.findElement(By.xpath("//div[@class='z0']"));
+	  write.click();
+	  WebElement to = driver.findElement(By.xpath("//textarea[@id=:9n]"));
+	  to.click();
+	  to.sendKeys(USER_2);
+	  to.submit();
 	  
-	  
-	 
+	 //driver.quit();
     //Assert.assertFalse("".equals(homepage.header.getText()));
   }
 }
